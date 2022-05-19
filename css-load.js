26743021bb96a0584c51b54193c0a630c0507007
GalaxyShard <76917584+GalaxyShard@users.noreply.github.com>
2022-05-19 14:22:53 -0400

    fixed bug caused by cache

--- css-load.js
+++ css-load.js
@@ -1,5 +1,6 @@
 window.addEventListener("pageshow", function()
 {
+    document.documentElement.classList.remove("page-unload");
     document.documentElement.classList.add("page-load");
 });
 window.addEventListener("beforeunload", function()
